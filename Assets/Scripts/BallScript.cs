9b47b1b2cba9e814466ffe10f44ec6500863dddc
Talha <talhacelik1907g@gmail.com>
2022-01-19 15:57:54 +0300

    0.1.1
    Completed Level 1 design. Completed mechanics and Game Manager script. Main menu does not exist.

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -4,51 +4,128 @@ using UnityEngine;
 
 public class BallScript : MonoBehaviour
 {
+    public Manager managerScript;
     public GameObject platform;
+    public GameObject cam;
+    float jumpModifier = 1.0f;
+
+    float lastY;
+    float thisY;
+    float deltaY;
+    Vector3 ballRbSpeed;
+
+    Vector3 camPos;
+    bool letSlide = false;
 
     Rigidbody ballRb;
     float startX;
     float deltaX;
     float rotationSpeed = 0.4f;
     string key;
+    bool isLive = true;
 
     void Start()
     {
         ballRb = gameObject.GetComponent<Rigidbody>();
     }
-
-
     void Update()
     {
-        if (Input.touchCount != 0)
+        if (isLive)
         {
-            Touch touch = Input.GetTouch(0);
+            if (Input.touchCount != 0)
+            {
+                Touch touch = Input.GetTouch(0);
 
-            if (touch.phase == TouchPhase.Began)
-            {
-                startX = touch.position.x;
-            }
-            else if (touch.phase== TouchPhase.Moved)
-            {
-                deltaX += (-(touch.position.x - startX) * rotationSpeed);
-                if (deltaX < -360f)
+                if (touch.phase == TouchPhase.Began)
                 {
-                    deltaX += 360f;
+                    startX = touch.position.x;
                 }
-                platform.transform.rotation= Quaternion.Euler(0f, deltaX, 0f);
-                startX = touch.position.x;
+                else if (touch.phase== TouchPhase.Moved)
+                {
+                    deltaX += (-(touch.position.x - startX) * rotationSpeed);
+                    if (deltaX < -360f)
+                    {
+                        deltaX += 360f;
+                    }
+                    else if (deltaX > 360f)
+                    {
+                        deltaX -= 360f;
+                    }
+                    platform.transform.rotation= Quaternion.Euler(0f, deltaX, 0f);
+                    startX = touch.position.x;
+                }
             }
         }
-        
+        Debug.Log(ballRb.velocity.y);
     }
+    void FixedUpdate()
+    {
+        if (isLive)
+        {
+            ballRbSpeed = ballRb.velocity;
+            ballRbSpeed.y = Mathf.Clamp(ballRbSpeed.y,-8f,50f);
+            ballRb.velocity = ballRbSpeed;
+            if (letSlide)
+            {
+                camSlide();
+            }
+        }
+
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
-        ballRb.AddForce(Vector3.up * 250f);
-        Debug.Log(collision.transform.name);
-
+        if (collision.transform.CompareTag("Plane"))
+        {
+            if (isLive)
+            {
+                camLet(0);
+                ballRb.velocity = Vector3.zero;
+                ballRb.AddForce(Vector3.up * 250f * jumpModifier);
+            }
+        }
+        else if (collision.transform.CompareTag("Fail"))
+        {
+            isLive = false;
+            ballRb.velocity = Vector3.zero;
+            managerScript.levelFailed();
+        }
+        else if (collision.transform.CompareTag("Pass"))
+        {
+            isLive = false;
+            ballRb.velocity = Vector3.zero;
+            managerScript.levelCompleted();
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
+        if (letSlide == false)
+        {
+            camLet(1);
+        }
+    }
+    
+    void camLet(int x)
+    {
         
+        if (x == 1)
+        {
+            letSlide = true;
+            lastY = this.transform.position.y;
+        }
+        else
+        {
+            letSlide = false;
+        }
+    }
+
+    void camSlide()
+    {
+        thisY = this.transform.position.y;
+        deltaY = thisY - lastY;
+        camPos = cam.transform.position;
+        camPos.y = camPos.y + deltaY;
+        cam.transform.position = camPos;
+        lastY = thisY;
     }
 }
---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Manager : MonoBehaviour
+{
+    public BallScript scriptBall;
+    public GameObject failedPanel;
+    public GameObject passPanel;
+
+    void Start()
+    {
+        
+    }
+
+    public void toNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void toMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    public void tryAgain()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void levelCompleted()
+    {
+        passPanel.SetActive(true);
+    }
+    public void levelFailed()
+    {
+        failedPanel.SetActive(true);
+    }
+}
